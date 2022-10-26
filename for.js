1316ab72d3d425d7dd21b0b383b96cd5d23bace2
Renatta Pereira <renatta.lll@hotmail.com>
2022-10-26 15:00:01 -0300

    for clássico/laço de repetição

---
+++ for.js
@@ -0,0 +1,9 @@
+// indices:      [0]    [1]     [2]   [3]     [4]   [5]
+const numeros = ["100", "200", "300", "400", "500", "600"];
+
+//                          length = 6
+for (let i = 0; i < numeros.length; i++) {
+  console.log(i, numeros[i]);
+}
+
+// O laço de repetição irá do índice 1 ao 5.
